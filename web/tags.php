2439543d2045b3529ac73c0dfe2f479e4c4fb4d3
Dionnatas Filho <dionnatas@dionnatas.com>
2020-01-29 22:11:07 -0300

    Correção do Icone

--- web/tags.php
+++ web/tags.php
@@ -5,12 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!--<meta name="viewport" content="width=device-width, initial-scale=1"> -->
     <title>Dionnatas.com</title>
-    <link rel="icon" type="image/png" href="/images/icon.png" />
+    <link rel="icon" type="image/png" href="icon.ico" />
     <!-- Bootstrap -->
     <link href="css/bootstrap.css" rel="stylesheet">
 
     <!-- HTML5 shim e Respond.js para suporte no IE8 de elementos HTML5 e media queries -->
-    <!-- ALERTA: Respond.js não funciona se você visualizar uma página file:// -->
+    <!-- ALERTA: Respond.js nï¿½o funciona se vocï¿½ visualizar uma pï¿½gina file:// -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
